d06659cb9c5e340185062677956f353ce0897581
Mathilda Thompson <mathildathompson288@unknown-b8:8d:12:32:29:02.home>
2013-06-23 19:55:27 +0100

    Changed the nav bar so that it is different for each page and added the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423203005) do
+ActiveRecord::Schema.define(:version => 20130623185433) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.text     "body"
+    t.integer  "recipe_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
